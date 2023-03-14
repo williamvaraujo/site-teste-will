08512d2d5a0ba6a0a2ba6410bebd1dc354e52a9d
williamvaraujo <114265989+williamvaraujo@users.noreply.github.com>
2023-03-13 21:50:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,13 @@ app = Flask(__name__)
 @app.route("/")
 def hell_world():
   return "Olá, vamos dominar o mundo! (Pink e Cérebro)"
+
+
+@app.route("/sobre")
+def sobre():
+  return "Aqui vamos postar o conteúdo de como dominar o mundo"
+
+
+@app.route("/contato")
+def contato():
+  return "Aqui colocaremos o contato dos dominadores de mundos mais experientes do mundo"

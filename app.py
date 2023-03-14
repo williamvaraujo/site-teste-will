9680407f8c4c15eb064136a7f34fdf696c9826b0
williamvaraujo <114265989+williamvaraujo@users.noreply.github.com>
2023-03-13 22:09:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,16 +2,22 @@ from flask import Flask
 
 app = Flask(__name__)
 
+menu = """
+
+<a href="/">Página inicial</a> | <a href="sobre">Sobre</a> | <a href="/contato">Contato</a>
+</br>
+"""
+
 @app.route("/")
 def hell_world():
-  return "Olá, vamos dominar o mundo! (Pink e Cérebro)"
+  return menu + "Olá, vamos dominar o mundo! (Pink e Cérebro)"
 
 
 @app.route("/sobre")
 def sobre():
-  return "Aqui vamos postar o conteúdo de como dominar o mundo"
+  return menu + "Aqui vamos postar o conteúdo de como dominar o mundo"
 
 
 @app.route("/contato")
 def contato():
-  return "Aqui colocaremos o contato dos dominadores de mundos mais experientes do mundo"
+  return menu + "Aqui colocaremos o contato dos dominadores de mundos mais experientes do mundo"
